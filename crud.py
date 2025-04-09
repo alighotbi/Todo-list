471abed414fbed862516bd37240b5928ba2524f3
alighotbi <mraligh95@gmail.com>
2025-04-09 19:48:01 +0330

    CRUD created

---
+++ crud.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+from  .models import Todo
+import schemas
+
+# Gets all the todo items from the database
+def get_all_todos(db:Session):
+    return db.query(Todo).all()
+
+# Gets a single todo item by ID.
+def get_todo(db:Session, todo_id : int):
+    return db.query(Todo).filter(Todo.id==todo_id).first()
+
+# Adds a new todo to the database.
+def create_todo(db:Session, todo : schemas.TodoCreate):
+    db_todo = Todo(**todo.model_dump())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+def update_todo(db:Session, todo_id : int, todo : schemas.TodoCreate ):
+    db_todo = get_todo(db, todo_id)
+    if db_todo:
+        db_todo.title = todo.title
+        db_todo.description = todo.description
+        db_todo.completed = todo.completed
+        db.commit()
+        db.refresh(db_todo)
+    return db_todo
+
+
+def delete_todo(db:Session, todo_id : int):
+    todo = get_todo(db, todo_id)
+    if todo:
+        db.delete(todo)
+        db.commit()
+    return todo
